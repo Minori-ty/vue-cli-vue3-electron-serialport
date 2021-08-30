f50741aabdaebb9aa55a8a4608deeb6f9e2afda7
XuZehong <xuzehong@sat-tech.cn>
2021-08-30 14:45:22 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,56 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <h1>串口调试</h1>
+    <button @click="open">打开串口</button>
+    <button @click="close">关闭串口</button>
 </template>
+<script setup>
+import serialport from 'serialport'
+var COM3 = null
+var COM2 = null
+var timer = null
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+const close = () => {
+    COM3.close((e) => {
+        console.log(e)
+    })
+    COM2.close((e) => {
+        console.log(e)
+    })
+}
+
+const open = () => {
+    COM3 = new serialport(
+        'COM3',
+        {
+            baudRate: 9600, //波特率
+            dataBits: 8, //数据位
+            parity: 'none', //奇偶校验
+            stopBits: 1, //停止位
+            flowControl: false,
+        },
+        false
+    )
+    COM2 = new serialport(
+        'COM2',
+        {
+            baudRate: 9600, //波特率
+            dataBits: 8, //数据位
+            parity: 'none', //奇偶校验
+            stopBits: 1, //停止位
+            flowControl: false,
+        },
+        false
+    )
+    clearInterval(timer)
+    var a = 1
+    timer = setInterval(() => {
+        COM2.write(`COM2发送的数据---${a}`)
+        a++
+    }, 1000)
+
+    COM3.on('readable', () => {
+        console.log('COM3接收到COM2发送的数据：', COM3.read().toString())
+    })
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style></style>
