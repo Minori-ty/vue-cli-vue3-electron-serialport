519fc68c1adb9e156e7a43020fe820c213bed153
XuZehong <xuzehong@sat-tech.cn>
2021-09-10 15:45:10 +0800

    fix(bug)修复bug

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,73 @@
 <template>
     <h1>串口调试</h1>
-    <button @click="open">打开串口</button>
-    <button @click="close">关闭串口</button>
+    <button @click="openCom2('COM2')">打开COM2</button>
+    <button @click="openCom3('COM3')">打开COM3</button>
+    <button @click="closeCOM2">关闭COM2</button>
+    <button @click="closeCOM3">关闭COM3</button>
 </template>
 <script setup>
-import serialport from 'serialport'
+import iconv from 'iconv-lite'
+import { openPort } from '../hooks'
 var COM3 = null
 var COM2 = null
 var timer = null
 
-const close = () => {
-    COM3.close((e) => {
-        console.log(e)
-    })
-    COM2.close((e) => {
-        console.log(e)
-    })
+const closeCOM2 = () => {
+    if (COM2?.isOpen) {
+        console.log('关闭COM2')
+        COM2.close(() => {})
+        console.log(COM2.isOpen)
+        return
+    }
+    console.log('COM2已关闭，请勿重复关闭')
+}
+const closeCOM3 = () => {
+    if (COM3?.isOpen) {
+        console.log('关闭COM3')
+        COM3.close(() => {})
+        console.log(COM3.isOpen)
+        return
+    }
+    console.log('COM3已关闭，请勿重复关闭')
 }
 
-const open = () => {
-    COM3 = new serialport(
-        'COM3',
-        {
-            baudRate: 9600, //波特率
-            dataBits: 8, //数据位
-            parity: 'none', //奇偶校验
-            stopBits: 1, //停止位
-            flowControl: false,
-        },
-        false
-    )
-    COM2 = new serialport(
-        'COM2',
-        {
-            baudRate: 9600, //波特率
-            dataBits: 8, //数据位
-            parity: 'none', //奇偶校验
-            stopBits: 1, //停止位
-            flowControl: false,
-        },
-        false
-    )
-    clearInterval(timer)
-    var a = 1
-    timer = setInterval(() => {
-        COM2.write(`COM2发送的数据---${a}`)
-        a++
-    }, 1000)
+function openCom2(port) {
+    if (!COM2?.isOpen) {
+        try {
+            console.log('打开COM2')
+            COM2 = openPort(port)
+            clearInterval(timer)
+            var a = 1
+            timer = setInterval(() => {
+                COM2.write(`COM2发送的数据---${a}`)
+                a++
+            }, 1000)
+        } catch (e) {
+            console.log('无法打开COM2，请检查是否在其他设备打开了COM2')
+        }
 
-    COM3.on('readable', () => {
-        console.log('COM3接收到COM2发送的数据：', COM3.read().toString())
-    })
+        return
+    }
+    console.log('COM2已打开，请勿重复打开')
+}
+
+function openCom3(port2) {
+    if (!COM3?.isOpen) {
+        console.log('打开COM3')
+        COM3 = openPort(port2)
+        if (COM2?.isOpen) {
+            COM3.on('readable', () => {
+                console.log('COM3接收到COM2发送的数据：', COM3.read().toString())
+            })
+        } else {
+            COM3.on('readable', () => {
+                console.log('COM3接收到COM2发送的数据：', iconv.decode(COM3.read(), 'gbk'))
+            })
+        }
+
+        return
+    }
+    console.log('COM3已打开，请勿重复打开')
 }
 </script>
 <style></style>
